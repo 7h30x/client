65d184e7f7039f3d2163946c37e1d4deb21c09fc
7h30x <theo.darmawan@gmail.com>
2019-02-25 23:51:42 +0700

    profile page v1

--- App.js
+++ App.js
@@ -4,14 +4,21 @@ import { ApolloProvider } from 'react-apollo'
 import { client } from './graphql/config'
 import { Root } from 'native-base'
 import RootNav from './navigations/RootNavigator'
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost'
+const client = new ApolloClient({ uri: 'https://timol-server.hafrizresa.pro/graphql' })
 
 export default class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
+<<<<<<< HEAD
         <Root>
           <RootNav />
         </Root>
+=======
+        <RootNav/>
+>>>>>>> profile page v1
       </ApolloProvider>
     );
   }
--- navigations/RootNavigator.js
+++ navigations/RootNavigator.js
@@ -2,21 +2,21 @@ import React from 'react';
 import {createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import {Ionicons, FontAwesome, MaterialCommunityIcons} from 'react-native-vector-icons'
 import BlankPage from '../screens/Blank'
-
-import Timbangan from './TimbanganNav'
-
+import StatsPage from '../screens/StatsPage'
 const  DashboardNav= createStackNavigator({
   Dashboard: BlankPage
 }, {
   initialRouteName: 'Dashboard'
 })
-
+const ProfileNav = createStackNavigator({
+  Stats: StatsPage
+})
 const  MainNavigator = createBottomTabNavigator({
   Home: DashboardNav,
-  Measure: Timbangan,
-  Profile: BlankPage,
+  Measure: BlankPage,
+  Profile: ProfileNav
 }, {
-  initialRouteName: 'Home',
+  initialRouteName: 'Profile',
   tabBarOptions: {
     activeTintColor: '#2e94b5',
     showLabel: false,
